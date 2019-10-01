e84b282d884c230c77a6680ec4870af49501ba41
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-10-01 13:57:10 +0930

    add admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+html,
+body {
+  font-family: "Open Sans", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
