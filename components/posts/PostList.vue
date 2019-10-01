39af4f59e9a65319c14d25e5328cd1ad3d32ebae
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-10-01 13:48:55 +0930

    add post form

--- components/posts/PostList.vue
+++ components/posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://www.fool.com.au/wp-content/uploads/2019/03/AI-circuit-board-tech-16.9.jpg"
       title="Hello there!"
       previewText="This is my first post!"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg"
       title="Post title 2"
       previewText="This is second post!"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg"
       title="Post title 3"
       previewText="This is third post!"
@@ -27,6 +30,11 @@ import PostPreview from "@/components/posts/PostPreview";
 export default {
   components: {
     PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean
+    }
   }
 };
 </script>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/admin/AdminPostForm";
+
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Kalyan",
+        title: "My awesome post",
+        content: "Super amazing....",
+        thumbnailLink:
+          "https://www.brookings.edu/wp-content/uploads/2017/11/metro_20171121_tech-empowers-tech-polarizes-mark-muro.jpg"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
